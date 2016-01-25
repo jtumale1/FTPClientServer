a5d6724f582f242937d043039053c46163bffeff
Montana <montanawong@gmail.com>
2016-01-24 20:18:39 -0500

    server code

--- FTP/src/Server.java
+++ FTP/src/Server.java
@@ -1,6 +1,25 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.net.Socket;
 
 public class Server {
 
+	private ExecutorService threadPool = Executors.newCachedThreadPool();
+	private int port;
+	private String address;
+	private Socket socket;
+	
+	public Server(String address, int port){
+		this.address = address;
+		this.port = port;
+	}
+	
+	public void run(){
+		//listen at a port
+		
+	}
+
+	
 	public static void main(String[] args) {
 		
 	}
