c51728397d6a5a038407998e5a19896d6b5cc6fc
jtumale1 <jtumale1@uga.edu>
2016-01-24 20:13:21 -0500

    serverthread

--- FTP/src/ServerThread.java
+++ FTP/src/ServerThread.java
@@ -1,3 +1,4 @@
+import java.io.File;
 
 public class ServerThread implements Runnable {
 	
@@ -6,4 +7,32 @@ public class ServerThread implements Runnable {
 		
 	}
 
+	public void cd(){
+		
+	}
+	
+	
+	public void ls(){
+		File curDir = new File(".");
+		File[] filesList = curDir.listFiles();
+	     for(File f : filesList){
+	    
+	            if(f.isFile()){
+	                System.out.println(f.getName());
+	            }
+	        }
+	}
+	
+	public void ls(String path){
+		File curDir = new File(path);
+		File[] filesList = curDir.listFiles();
+	     for(File f : filesList){
+	    
+	            if(f.isFile()){
+	                System.out.println(f.getName());
+	            }
+	        }
+		
+	}
+	
 }
