273ebb60d2a7be82bc58699fdaba4a92ed0ec100
Montana <montanawong@gmail.com>
2016-01-27 10:40:29 -0500

    implment client

--- FTP/src/Client.java
+++ FTP/src/Client.java
@@ -1,40 +1,53 @@
+import java.io.IOException;
+import java.io.PrintStream;
 import java.net.*;
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 
 public class Client {
 	
+	private ExecutorService threadPool = Executors.newCachedThreadPool();
 	private Socket clientSocket = null;
 	private Scanner scanner = new Scanner(System.in);
 	private PrintStream output = null;
+	private int connectPort;
+	private String hostName = null;
 	
 	public Client(int port){
-		clientSocket = new Socket("localhost", port);
+		this.connectPort = port;
+		this.hostName = "localhost";
 	}
-	public Client(String hostname, int port){
-		clientSocket = new Socket(hostname, port);
+	public Client(String hostName, int port){
+		this.connectPort = port;
+		this.hostName = hostName;
 	}
 	
 	
-	public void run(){
+	public void run() throws UnknownHostException, IOException{
+		//create socket
+		this.clientSocket = new Socket(this.hostName, this.connectPort);
+		
+		//read commands from sys.in
 		String input = null;
 		while(true){
 			System.out.print("ftpclient> ");
 			input = this.scanner.nextLine();
-			if(input.equalsIgnoreCase("quit") 
+			if(input.equalsIgnoreCase("quit")){
+				//close connection 
 				break;
-			else if this.parse(input) == true(){
-				//TODO use threadpool
-				ClientThread clientThread = new ClientThread(clientSocket, input);
+				
 			}
 			else{
-				//print error message
+				//TODO use threadpool
+				ClientThread clientThread = new ClientThread(this.clientSocket, input);
+				this.threadPool.execute(clientThread);
 			}
+			
 		}
 	}
 	
-	private boolean parse(){
-		//todo
-	}
 	
 	
 	public static void main(String[] args){
